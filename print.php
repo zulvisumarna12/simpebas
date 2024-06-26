9b8a46fe81263e7bb2508286e4944cdca59815b5
Zulvi Sumarna <zulvideveloper@gmail.com>
2024-06-26 15:28:16 +0700

    first commit

---
+++ print.php
@@ -0,0 +1,67 @@
+<?php 
+    require 'config.php';
+    include $view;
+    $lihat = new view($config);
+    $toko = $lihat -> toko();
+    $hsl = $lihat -> penjualan();
+?>
+<html>
+    <head>
+        <title>print</title>
+        <link rel="stylesheet" href="assets/css/bootstrap.css">
+    </head>
+    <body>
+        <script>window.print();</script>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4"></div>
+                <div class="col-sm-4">
+                    <center>
+                        <p><?php echo $toko['nama_toko'];?></p>
+                        <p><?php echo $toko['alamat_toko'];?></p>
+                        <p>Tanggal : <?php  echo date("j F Y, G:i");?></p>
+                        <p>Kasir : <?php  echo $_GET['nm_member'];?></p>
+                    </center>
+                    <table class="table table-bordered" style="width:100%;">
+                        <tr>
+                            <td>No.</td>
+                            <td>Barang</td>
+                            <td>Jumlah</td>
+                            <td>Total</td>
+                        </tr>
+                        <?php $no=1; foreach($hsl as $isi){?>
+                        <tr>
+                            <td><?php echo $no;?></td>
+                            <td><?php echo $isi['nama_barang'];?></td>
+                            <td><?php echo $isi['jumlah'];?></td>
+                            <td><?php echo $isi['total'];?></td>
+                        </tr>
+                        <?php $no++; }?>
+                    </table>
+                    <div class="pull-right">
+                        <?php 
+                            $hasil = $lihat -> jumlah(); 
+                            $total_bayar = $hasil['bayar'];
+                            $pajak = $total_bayar * 0.10; // 10% tax
+                            $total_setelah_pajak = $total_bayar - $pajak;
+                        ?>
+                        Total : Rp.<?php echo number_format($total_bayar);?>,-
+                        <br/>
+                        Pajak PPN (11%) : Rp.<?php echo number_format($pajak);?>,-
+                        <br/>
+                        Total Setelah Pajak PPN : Rp.<?php echo number_format($total_setelah_pajak);?>,-
+                        <br/>
+                        Bayar : Rp.<?php echo number_format($_GET['bayar']);?>,-
+                        <br/>
+                        Kembali : Rp.<?php echo number_format($_GET['kembali']);?>,-
+                    </div>
+                    <div class="clearfix"></div>
+                    <center>
+                        <p>Terima Kasih Telah berbelanja di toko kami !</p>
+                    </center>
+                </div>
+                <div class="col-sm-4"></div>
+            </div>
+        </div>
+    </body>
+</html>
